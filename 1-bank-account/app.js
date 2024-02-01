0328130e4da0e8dc1708b4f1b67434ada3152f98
Remi Ogunsanya <50562900+amalaboi@users.noreply.github.com>
2024-02-01 20:18:42 +0000

    Update app.js

--- 1-bank-account/app.js
+++ 1-bank-account/app.js
@@ -18,14 +18,36 @@ class BankAccount {
   }
   // amt = amount
   deposit(amt) {
-    this.balance = this.balance + amt;
+    //   only deposit if amount > 0... you can deposit negative numbers
+    if (amt > 0) {
+      console.log(`NAME: ${this.accountHolder}`);
+      console.log(`ACCOUNT NUMBER: ${this.accountNumber}`);
+      this.balance = this.balance + amt;
+      console.log(`You deposited £${amt}.  `);
+      console.log(`New balance is: £${this.balance}`);
+    } else {
+      console.log("Can't deposit a negative amount");
+    }
   }
 
   withdraw(amt) {
+    console.log(`NAME: ${this.accountHolder}`);
+    console.log(`ACCOUNT NUMBER: ${this.accountNumber}`);
     if (this.balance <= 0 || amt > this.balance) {
       console.log("You have insufficient funds");
     } else {
       this.balance = this.balance - amt;
+      console.log(`You withdrew £${amt}.  `);
+      console.log(`New balance is: £${this.balance}`);
     }
   }
 }
+
+// test
+let bankerA = new BankAccount("345de2wsq", "Segun Oguntade", 200);
+let bankerB = new BankAccount("Klem23455sd", "Ganiyu Gbadamosi");
+
+bankerA.withdraw(75.34);
+bankerB.withdraw(90);
+bankerB.deposit(160);
+bankerB.withdraw(90);
